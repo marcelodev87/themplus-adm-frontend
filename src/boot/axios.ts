b95def552165b7f87701ec1ac2b8d985b991c940
Carlos Davi <carlosdavi004@gmail.com>
2025-03-19 00:31:29 -0300

    Feat: Alterações na configuração do axios

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,5 +1,8 @@
-import { defineBoot } from '#q-app/wrappers';
-import axios, { type AxiosInstance } from 'axios';
+import { boot } from 'quasar/wrappers';
+import type { AxiosInstance } from 'axios';
+import axios from 'axios';
+import { useAuthStore } from 'src/stores/auth-store';
+import { storeToRefs } from 'pinia';
 
 declare module 'vue' {
   interface ComponentCustomProperties {
@@ -8,24 +11,25 @@ declare module 'vue' {
   }
 }
 
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' });
+const api = axios.create({ baseURL: process.env.VITE_API_BASE_URL || 'http://localhost:8000' });
 
-export default defineBoot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
+export default boot(({ app }) => {
+  const authStore = useAuthStore();
+  const { token: tokenValue } = storeToRefs(authStore);
+
+  api.interceptors.request.use(
+    (config) => {
+      const token = tokenValue.value;
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error instanceof Error ? error : new Error(String(error))),
+  );
 
   app.config.globalProperties.$axios = axios;
-  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
-  //       so you won't necessarily have to import axios in each vue file
-
   app.config.globalProperties.$api = api;
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
 });
 
 export { api };
